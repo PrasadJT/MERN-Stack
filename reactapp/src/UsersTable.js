60c0b1c11bf1d222e21fbc360ff8521f7064d7a2
Thilina Prasad <thilina1091@gmail.com>
2024-08-02 01:29:05 +0530

    Create UsersTable.js

---
+++ reactapp/src/UsersTable.js
@@ -0,0 +1,20 @@
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+
+const UsersTable = props =>{
+    <TableContainer component={Paper}>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+
+            </TableBody>
+        </Table>
+    </TableContainer>
+}
+
+export default UsersTable;
