8077f0b3349f08b2e1d5b30bdce65242efb74c60
Thilina Prasad <thilina1091@gmail.com>
2024-08-02 22:15:23 +0530

    completed user table

--- reactapp/src/Users.js
+++ reactapp/src/Users.js
@@ -1,8 +1,30 @@
+import { Box } from "@mui/material";
 import UserForm from "./UserForm";
+import UsersTable from "./UsersTable";
+
+const users = [
+    {
+        id: 1,
+        name: 'sunil',
+    },
+    {
+        id: 2,
+        name: 'kasun',
+    }
+];
 
 const Users = () => {
     return(
-        <UserForm></UserForm>
+        <Box
+            sx={{
+                width: 'calc(100% - 100px)',
+                margin: 'auto',
+                marginTop: '100px',
+            }}
+        >
+            <UserForm />
+            <UsersTable rows={users}/>
+        </Box>
     );
 }
 
